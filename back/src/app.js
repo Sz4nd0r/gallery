45dc15cb24b08b16980d3d148cb458a9e6556d58
Sz4nd0r <penaszandor@gmail.com>
2025-11-27 19:16:08 -0400

    feature: CRUD APIs

--- back/src/app.js
+++ back/src/app.js
@@ -6,9 +6,12 @@ app.use(express.json())
 
 // importing routes
 import userRouter from './routes/user.route.js'
+import postRouter from './routes/post.route.js'
 
 // declaring routes
 app.use("api/v1/users", userRouter)
+app.use("api/v1/posts", postRouter)
+
 
 // exaple route http://localhost:4000/api/v1/user/register
 
---
+++ back/src/routes/post.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
+
+const router = Router();
+
+router.route("/create").post(createPost);
+router.route("/getPosts").get(getPosts);
+router.route("/updatePost/:id").patch(updatePost)
+router.route("/deletePost/:id").delete(deletePost)
+
+export default router
+
